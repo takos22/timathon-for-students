b32b2832b33bc6507dfd2b2341d18bae392e2689
Takos22 <tora.akos@gmail.com>
2020-10-07 21:29:34 +0200

    Update db handling for dev and prod

--- website/settings.py
+++ website/settings.py
@@ -79,20 +79,24 @@ WSGI_APPLICATION = "website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("POSTGRE_DBNAME"),
-        "USER": os.environ.get("POSTGRE_USERNAME"),
-        "PASSWORD": os.environ.get("POSTGRE_PASSWORD"),
-        "HOST": os.environ.get("POSTGRE_HOST"),
-        "PORT": os.environ.get("POSTGRE_PORT"),
-    },
-    "sqlite": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-}
+if os.environ.get("ENV", "DEV") == "PROD":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ.get("POSTGRE_DBNAME"),
+            "USER": os.environ.get("POSTGRE_USERNAME"),
+            "PASSWORD": os.environ.get("POSTGRE_PASSWORD"),
+            "HOST": os.environ.get("POSTGRE_HOST"),
+            "PORT": os.environ.get("POSTGRE_PORT"),
+        },
+    }
+else:
+    DATABASES = {
+        "sqlite": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+    }
 
 
 # Password validation
