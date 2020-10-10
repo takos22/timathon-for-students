17b088edb01837399ad705e4105929ec57a91591
Takos22 <tora.akos@gmail.com>
2020-10-10 19:54:08 +0200

    Update api view to return endpoints

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the api index.")
+    return JsonResponse({"Endpoints": ["get_homework_by_id"]})
