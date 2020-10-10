cd72750dc60cbab7729863ad7dd6d622e7dd94b8
Takos22 <tora.akos@gmail.com>
2020-10-10 20:09:07 +0200

    Add homework api with rest framework

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Homework
+
+
+class HomeworkSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Homework
+        fields = ("id", "subject", "name", "description", "added_date", "due_date", "finished")
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register("homework", views.HomeworkViewSet)
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", include(router.urls)),
+    path("endpoints", views.endpoints),
 ]
