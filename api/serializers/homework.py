f19f66b86bdd299168c140d1365c0eb785aae78e
Takos22 <tora.akos@gmail.com>
2020-10-18 21:50:35 +0200

    Update homework serializer

--- api/serializers/homework.py
+++ api/serializers/homework.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 
 from ..models import Homework
 
+from .user import UserSerializer
 
-class HomeworkSerializer(serializers.HyperlinkedModelSerializer):
+
+class HomeworkSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Homework
-        fields = ("id", "subject", "name", "description", "added_date", "due_date", "finished")
+        fields = ("id", "user", "subject", "name", "description", "added_date", "due_date", "finished")
