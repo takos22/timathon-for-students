036861ea8bc899e44b315629ee7cc9faa2b5959e
Takos22 <tora.akos@gmail.com>
2020-10-18 19:55:57 +0200

    Add user to Homework model

--- api/models/homework.py
+++ api/models/homework.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 class Homework(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=128)
     name = models.CharField(max_length=128)
     description = models.TextField()
