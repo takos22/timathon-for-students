45e9a02f6f9b86085ebbda962b33f11d0cd35075
Takos22 <tora.akos@gmail.com>
2020-10-18 22:26:40 +0200

    Update user view set

--- api/views/user.py
+++ api/views/user.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import User
 
 from ..serializers import UserSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return User.objects.all() if self.request.user.is_superuser else User.objects.filter(id=self.request.user.id)
+        return (
+            User.objects.all().order_by("id")
+            if self.request.user.is_superuser
+            else User.objects.filter(id=self.request.user.id)
+        )
