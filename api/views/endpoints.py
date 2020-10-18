ee417b63c7735b37362806ceffef976d899244fe
Takos22 <tora.akos@gmail.com>
2020-10-18 21:05:17 +0200

    Update endpoints

--- api/views/endpoints.py
+++ api/views/endpoints.py
@@ -1,4 +1,13 @@
 from django.http import JsonResponse
 
+
 def endpoints(request):
-    return JsonResponse({"endpoints": ["homework"]})
+    return JsonResponse(
+        {
+            "endpoints": {
+                "endpoints": "Get all the endpoints of the API.",
+                "homework": "Get a list of all the user's homeworks.",
+                "homework/<int:pk>": "Get a user's homework by its id.",
+            }
+        }
+    )
