674b2454be4e532ddca269c8e3a707207e81c5f7
Takos22 <tora.akos@gmail.com>
2020-10-18 21:50:23 +0200

    Update user serializer

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from .homework import HomeworkSerializer
-
 
 class UserSerializer(serializers.ModelSerializer):
-    homeworks = HomeworkSerializer(many=True)
-
     class Meta:
         model = User
-        fields = ("id", "username", "homeworks")
+        fields = ("id", "username", "email")
