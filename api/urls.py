09c66591a14665a8808cba6ff7e13486cd3156d2
Takos22 <tora.akos@gmail.com>
2020-10-19 13:19:47 +0200

    Add names to urls

--- api/urls.py
+++ api/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path("auth/", include("rest_framework.urls"), name="auth"),
 
     # Authentificated
-    path("homework/", views.homework_list),
-    path("homework/<int:pk>/", views.homework_retrieve),
+    path("homework/", views.homework_list, name="homework-list"),
+    path("homework/<int:pk>/", views.homework_retrieve, name="homework-retrieve"),
 
     # Admin
-    path("user/", views.user_list),
-    path("user/<int:pk>/", views.user_retrieve),
+    path("user/", views.user_list, name="user-list"),
+    path("user/<int:pk>/", views.user_retrieve, name="user-retrieve"),
 ]
