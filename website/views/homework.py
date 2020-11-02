a81725f6963a6ee464a57904b9a19f92d9c8fa3a
Takos22 <tora.akos@gmail.com>
2020-11-02 21:10:16 +0100

    Update homework template

--- website/views/homework.py
+++ website/views/homework.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from ..models import Homework
+
 
 def homework(request):
-    return render(request, "homework.html")
+    homeworks = request.user.homeworks.all()
+    return render(request, "homework.html", dict(homeworks=homeworks))
