452b724c9996c39b90d7a7609076302823717b16
Takos22 <tora.akos@gmail.com>
2020-11-02 21:32:52 +0100

    Add templates to view

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,5 +8,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("homework/", views.homework, name="homework"),
     path("account/", views.account, name="account"),
+    path("account/login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("account/signup/", views.signup, name="signup"),
+    path("account/logout/", views.logout, name="logout"),
     # path("contact/", views.contact, name="contact"),
 ]
--- website/views/__init__.py
+++ website/views/__init__.py
@@ -2,3 +2,5 @@ from .index import index
 from .homework import homework
 from .account import account
 from .contact import contact
+from .signup import signup
+from .logout import logout_view as logout
