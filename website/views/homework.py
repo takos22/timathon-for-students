9a27c90b6a247fe51211213d4d7a6cd724dd0676
Takos22 <tora.akos@gmail.com>
2020-11-02 21:37:47 +0100

    Fix homework view bug

--- website/views/homework.py
+++ website/views/homework.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from ..models import Homework
 
 
 def homework(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
     homeworks = request.user.homeworks.all()
     return render(request, "homework.html", dict(homeworks=homeworks))
