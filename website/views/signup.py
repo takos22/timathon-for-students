e26d95f78dec7b63c97309d91e93e9529975231c
Takos22 <tora.akos@gmail.com>
2020-11-03 20:11:36 +0100

    Remve print debug

--- website/views/signup.py
+++ website/views/signup.py
@@ -5,20 +5,14 @@ from django.shortcuts import render, redirect
 
 def signup(request):
     if request.method == "POST":
-        print("post")
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            print("valid")
             form.save()
-            print("saved")
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
-            print("auth")
             login(request, user)
-            print("login")
             return redirect("index")
     else:
-        print("get")
         form = UserCreationForm()
     return render(request, "signup.html", {"form": form})
