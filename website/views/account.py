a67bbbe92c53d6687f051cb1a69258e1b14f6b37
Takos22 <tora.akos@gmail.com>
2020-11-03 20:39:50 +0100

    Update account

--- website/views/account.py
+++ website/views/account.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from ..models import Homework
 
 def account(request):
-    return render(request, "account.html")
+    if request.user.is_authenticated:
+        homeworks = Homework.objects.filter(user=request.user, finished=False)
+    else:
+        homeworks = []
+    return render(request, "account.html", dict(homeworks=homeworks))
